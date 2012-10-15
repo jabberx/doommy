004d26d2bffc2944fcf591cd50457348a0f4ba8c
jabberx <jabberx@ymail.com>
2012-10-15 15:41:44 +0200

    better formatting

--- examples/parts/generator1.lua
+++ examples/parts/generator1.lua
@@ -12,33 +12,33 @@ dofile('parts/fast_shade1.lua')
 dofile('parts/shadow1.lua')
 
 function generator1(xsize,ysize,zsize)
-	sca = 0.5
+  sca = 0.5
     for x=0,xsize-1 do
-		handle_sdl_events()
-		set_window_title("generator1 "..math.floor(x/xsize*50).."%")
+    handle_sdl_events()
+    set_window_title("generator1 "..math.floor(x/xsize*50).."%")
         for z=0,zsize-1 do
-			color = 255
-			height = perlin_noise_3d(sca*x/30,1,sca*z/30)*16 -
-					 math.abs(perlin_noise_3d(sca*x/5,10,sca*z/5)*4)
-			mountain = perlin_noise_3d(sca*x/15,77,sca*z/15)*8
-			if height+mountain > 0 then
-				height = height * 2
-				color = 245
-			end
-			mod2 = perlin_noise_3d(sca*x/15,20,sca*z/15)
-			if mod2>-0.1 then
-				mod2 = 0
-			end
-			height = height + mod2*4 + 32
+      color = 255
+      height = perlin_noise_3d(sca*x/30,1,sca*z/30)*16 -
+           math.abs(perlin_noise_3d(sca*x/5,10,sca*z/5)*4)
+      mountain = perlin_noise_3d(sca*x/15,77,sca*z/15)*8
+      if height+mountain > 0 then
+        height = height * 2
+        color = 245
+      end
+      mod2 = perlin_noise_3d(sca*x/15,20,sca*z/15)
+      if mod2>-0.1 then
+        mod2 = 0
+      end
+      height = height + mod2*4 + 32
             for y=0,ysize-1 do    
                 if math.abs(y-height)<4 then
-					set_voxel_pal(x,y,z,color-math.random(2))
-				end
+          set_voxel_pal(x,y,z,color-math.random(2))
+        end
             end 
         end 
-    end	
-	
-	sca = 1
+    end  
+  
+  sca = 1
     for x=0,xsize-1 do
     handle_sdl_events()
     set_window_title("generator1 "..math.floor(x/xsize*50).."%")
@@ -50,7 +50,7 @@ function generator1(xsize,ysize,zsize)
             end 
         end 
     end
-	
-	fast_shade1(xsize,ysize,zsize)	
-	shadow1(1000,4000,1000, 5, xsize,ysize,zsize)	
+  
+  fast_shade1(xsize,ysize,zsize)  
+  shadow1(1000,4000,1000, 5, xsize,ysize,zsize)  
 end
