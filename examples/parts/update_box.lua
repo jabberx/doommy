f393c8cd5982406eb61a95bf9d7db684ba81112c
jabberx <jabberx@ymail.com>
2012-10-19 00:56:19 +0200

    bug fix
    works fine near level borders now

--- examples/parts/update_box.lua
+++ examples/parts/update_box.lua
@@ -7,10 +7,14 @@
 ]]
 
 function update_box(x1,y1,z1,x2,y2,z2)
-  for x = x1-32,x2+32,16 do
-    for y = y1-32,y2+32,16 do
-      for z = z1-32,z2+32,16 do
-        update_chunk(x,y,z)
+  for x0 = x1-32,x2+32,16 do
+    for y0 = y1-32,y2+32,16 do
+      for z0 = z1-32,z2+32,16 do
+        if x0>0 and y0>0 and z0>0 then
+          update_chunk(math.floor(x0/32)*32,
+                       math.floor(y0/32)*32,
+                       math.floor(z0/32)*32)		
+        end
       end
     end
   end
