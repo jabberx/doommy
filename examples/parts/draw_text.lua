7bb5aa3b09ad3da2a9e3f6f0b955c7c441ab10d1
jabberx <jabberx@ymail.com>
2012-10-18 16:01:10 +0200

    bitmap font example added to parts

---
+++ examples/parts/draw_text.lua
@@ -0,0 +1,24 @@
+--[[
+                    draw_text.lua
+
+    example using bimtap font located in parts/courier14.bmp
+]]
+
+font_txt = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890-=\\!@#$%^&*()_+|[]{};:'\",./<>?`~ÀÁÂÃÄÅ¨ÆÇÈÉÊËÌÍÎÏĞÑÒÓÔÕÖ×ØÙÚÜİÛŞßàáâãäå¸æçèéêëìíîïğñòóôõö÷øùúüıûşÿ "
+char_w,char_h,char_m,char_cnt = 11,16,0,160
+font_img = load_image('parts/courier14.bmp',1)
+
+function draw_text(x,y,text)
+  for i=1, #text do
+    local ch = text:sub(i,i)
+    for c=1, #font_txt do
+      local fch = font_txt:sub(c,c)
+      if fch == ch then
+        if c<=char_cnt then
+          draw_image_part(x+i*(char_w+char_m),y,(c-1)*char_w,0,char_w,char_h,font_img)
+        end
+        break
+      end
+    end
+  end
+end
